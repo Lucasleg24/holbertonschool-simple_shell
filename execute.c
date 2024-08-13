04e615712772978b7f2b860fed237db6222dbbb7
Basco <martinez.xabi@laposte.net>
2024-08-13 18:57:12 +0000

    Add execute function

---
+++ execute.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+void execute(char **command, char **envp)
+{
+	char *actual_command = NULL;
+	pid_t pid;
+	int status, i;
+
+	actual_command = get_location(command[0], envp);
+
+	if (actual_command == NULL)
+	{
+		for (i = 0; command[i] != NULL; i++)
+			printf("%s ", command[i]);
+		printf("\n");
+		return;
+	}
+
+	pid = fork();
+	if (pid == -1)
+		perror("Shell: fork error");
+	else if (pid == 0)
+	{
+		if (execve(actual_command, command, envp) == -1)
+			perror("Shell");
+		exit(EXIT_FAILURE);
+	} else
+		wait(&status);
+
+	free(actual_command);
+}
