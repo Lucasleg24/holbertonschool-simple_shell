a768ecfa97dafc673c40cea713626feeb7116bef
Basco <martinez.xabi@laposte.net>
2024-08-20 10:03:45 +0000

    Add Valgrind tester

--- process_command.c
+++ process_command.c
@@ -13,7 +13,7 @@
 void process_command(char **args, char **envp, char *line)
 {
 	if (isatty(0) && strcmp(args[0], "exit") != 0)
-                print_history(line);
+		print_history(line);
 
 	if (strcmp(args[0], "exit") == 0)
 		shell_exit(line, args);
---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "ls" "cd" "/bin/ls" "/bin/ls" "test" "hello" "world" "azerty" "ls -l" "qwerty" "\n"
